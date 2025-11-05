2e99b17f275aa5c03716287dacb52de27b495948
Matthew <15275025+GlitchedMythos@users.noreply.github.com>
2025-11-04 18:32:03 -0600

    better-auth added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,63 +1,78 @@
-import { createFiberplane, createOpenAPISpec } from "@fiberplane/hono";
-import { eq } from "drizzle-orm";
-import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
-import * as schema from "./db/schema";
-import { ZUserByIDParams, ZUserInsert } from "./dtos";
-import { dbProvider } from "./middleware/dbProvider";
-import { zodValidator } from "./middleware/validator";
+import {
+  createFiberplane,
+  createOpenAPISpec,
+} from '@fiberplane/hono';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { HTTPException } from 'hono/http-exception';
+import { auth } from './lib/better-auth';
+import { dbProvider } from './middleware/dbProvider';
 
 const api = new Hono()
-  .use("*", dbProvider)
-  .get("/users", async (c) => {
-    const db = c.var.db;
-    const users = await db.select().from(schema.users);
-
-    return c.json(users);
-  })
-  .post("/users", zodValidator("json", ZUserInsert), async (c) => {
-    const db = c.var.db;
-    const { name, email } = c.req.valid("json");
-
-    const [newUser] = await db
-      .insert(schema.users)
-      .values({
-        name: name,
-        email: email,
-      })
-      .returning();
-
-    return c.json(newUser, 201);
-  })
-  .get("/users/:id", zodValidator("param", ZUserByIDParams), async (c) => {
-    const db = c.var.db;
-    const { id } = c.req.valid("param");
-
-    const [user] = await db
-      .select()
-      .from(schema.users)
-      .where(eq(schema.users.id, id));
-
-    if (!user) {
-      return c.notFound();
-    }
-
-    return c.json(user);
-  })
-  .delete("/users/:id", zodValidator("param", ZUserByIDParams), async (c) => {
-    const db = c.var.db;
-    const { id } = c.req.valid("param");
-
-    await db.delete(schema.users).where(eq(schema.users.id, id));
-
-    return c.body(null, 204);
+  .use('*', dbProvider)
+  .use(
+    '/api/auth/*', // or replace with "*" to enable cors for all routes
+    cors({
+      origin: 'http://localhost:5173', // replace with your origin
+      allowHeaders: ['Content-Type', 'Authorization'],
+      allowMethods: ['POST', 'GET', 'OPTIONS'],
+      exposeHeaders: ['Content-Length'],
+      maxAge: 600,
+      credentials: true,
+    })
+  )
+  .on(['GET', 'POST'], 'auth/*', async (c) => {
+    return auth(c.env as CloudflareBindings).handler(c.req.raw);
   });
+// .get("/users", async (c) => {
+//   const db = c.var.db;
+//   const users = await db.select().from(schema.users);
+
+//   return c.json(users);
+// })
+// .post("/users", zodValidator("json", ZUserInsert), async (c) => {
+//   const db = c.var.db;
+//   const { name, email } = c.req.valid("json");
+
+//   const [newUser] = await db
+//     .insert(schema.users)
+//     .values({
+//       name: name,
+//       email: email,
+//     })
+//     .returning();
+
+//   return c.json(newUser, 201);
+// })
+// .get("/users/:id", zodValidator("param", ZUserByIDParams), async (c) => {
+//   const db = c.var.db;
+//   const { id } = c.req.valid("param");
+
+//   const [user] = await db
+//     .select()
+//     .from(schema.users)
+//     .where(eq(schema.users.id, id));
+
+//   if (!user) {
+//     return c.notFound();
+//   }
+
+//   return c.json(user);
+// })
+// .delete("/users/:id", zodValidator("param", ZUserByIDParams), async (c) => {
+//   const db = c.var.db;
+//   const { id } = c.req.valid("param");
+
+//   await db.delete(schema.users).where(eq(schema.users.id, id));
+
+//   return c.body(null, 204);
+// });
 
 const app = new Hono()
-  .get("/", (c) => {
-    return c.text("Honc from above! ☁️🪿");
+  .get('/', (c) => {
+    return c.text('Honc from above! ☁️🪿');
   })
-  .route("/api", api);
+  .route('/api', api);
 
 app.onError((error, c) => {
   console.error(error);
@@ -66,15 +81,15 @@ app.onError((error, c) => {
       {
         message: error.message,
       },
-      error.status,
+      error.status
     );
   }
 
   return c.json(
     {
-      message: "Something went wrong",
+      message: 'Something went wrong',
     },
-    500,
+    500
   );
 });
 
@@ -82,14 +97,14 @@ app.onError((error, c) => {
  * Serve a simplified api specification for your API
  * As of writing, this is just the list of routes and their methods.
  */
-app.get("/openapi.json", (c) => {
+app.get('/openapi.json', (c) => {
   return c.json(
     createOpenAPISpec(app, {
       info: {
-        title: "HONC Neon App",
-        version: "1.0.0",
+        title: 'HONC Neon App',
+        version: '1.0.0',
       },
-    }),
+    })
   );
 });
 
@@ -99,11 +114,11 @@ app.get("/openapi.json", (c) => {
  * Visit the explorer at `/fp`
  */
 app.use(
-  "/fp/*",
+  '/fp/*',
   createFiberplane({
     app,
-    openapi: { url: "/openapi.json" },
-  }),
+    openapi: { url: '/openapi.json' },
+  })
 );
 
 export default app;
